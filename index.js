f43447746942e14de5a759aa58d10f4041f4df3b
Roberto <rob.vicalvaro@gmail.com>
2018-06-18 23:53:36 +0200

    Funcionando hasta el momento de la elección - incluido loading

--- index.js
+++ index.js
@@ -1,51 +1,141 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const prompt = inquirer.createPromptModule();
+const json = fs.readFileSync("data_base.json");
+const database = JSON.parse(json);
 
-let prompt = inquirer.createPromptModule();
+function randomQuestion(type, block) {
+    
+    let info = database[type].preguntas[block];
+    
+    var size = Object.getOwnPropertyNames(info);
 
-//Creamos el inicio del juego
+    let questionNum = Math.floor(Math.random() * size.length);
+    
+    let index = [];
+    
+    index.sort(function (a, b) {
+        return a == b ? 0 : (a > b ? 1 : -1);
+    });
 
-//Solo dos preguntas, aleatorias
+    for (var x in info) {
+        index.push(x);
+    }
+    return info[index[questionNum]];
+}
+
+function waitaMoment() {
+
+        var P = ["\\", "|", "/", "-"];
+        var x = 0;
+        console.log('Dejame pensar!');
+        return setInterval(function () {
+            process.stdout.write("\r" + P[x++]);
+            x &= 3;
+        }, 250);
+
+}
+
+// Creamos el inicio del juego
+// Por cada elemento, hay dos bloques, preguntas y respuestas,
+// Las preguntas tienen dos bloques primero lanza una pregunta del primer bloque que filtra un gran número de respuestas y despues lanza una pregunta aleatoria del segundo bloque que define mejor la respuesta
+
+
+// Bienvenida
 
 let wellcomeQuestion = [{
     message: 'Bienvenido al juego Animal Vegetal Mineral, ¿Quiere jugar?',
     type: 'confirm',
-    default: false,
+    default: true,
     name: 'letsPlay'
 }];
 
-// Obtenemos el jugador
 
-let getNamePlayer = [{
-    message: 'Por favor, dígame su nombre',
-    type: 'input',
-    name: 'namePlayer'
-}];
 
- 
-inquirer.prompt(wellcomeQuestion)
-.then(answers => {
-    if (!answers.letsPlay) {
-        console.log(`Vuelve cuando quieras! :) ${answers.letsPlay}`);
-        return;
-    }
-    continueExecution();
-})
-.catch( error => {
-    throw error;
-});
 
-function continueExecution() {
-    inquirer.prompt(getNamePlayer)
+prompt(wellcomeQuestion)
     .then(answers => {
-        if (answers.namePlayer.length === 0) {
-            answers.namePlayer = 'Tipo de incognito';
+        if (!answers.letsPlay) {
+            console.log(`Vuelve cuando quieras! :)`);
+            return;
         }
-        console.log(`Bienvenido al juego ${answers.namePlayer}`);
-        return true;
+        // Obtenemos el jugador
+
+        let continuePlay = [{
+            message: 'Por favor, dígame su nombre',
+            type: 'input',
+            name: 'namePlayer'
+        }];
+
+        continueExecution(continuePlay);
     })
     .catch( error => {
         throw error;
+    });
+
+
+// Seguimos con el juego
+
+function continueExecution(continuePlay) {
+
+    prompt(continuePlay)
+    .then(answers => {
+
+        //Si pulsamos enter sin introducir nombre, el nombre es tipo de incognito
+        if (answers.namePlayer.length === 0) {
+            answers.namePlayer = 'Tipo de incognito';
+        }
+
+        // Bienvenido!
+        console.log(`Bienvenido al juego ${answers.namePlayer}! 
+Con solo dos preguntas voy a adivinar lo que estás pensando. 
+Comencemos con la primera pregunta...`);
+        
+
+        // Preguntamos por animal vegetal mineral
+
+        let avmQuestion = [{
+            message: '¿En qué elemento está pensando?',
+            type: 'list',
+            name: 'avmType',
+            choices: ['animal', 'vegetal', 'mineral']
+        }];
+        
+
+        prompt(avmQuestion)
+            .then(answers => {
+
+            // Generamos y lanzamos la pregunta aleatoria del bloque 1
+            let typeQuestions = [{
+                message: randomQuestion(answers.avmType, 'bloque1'),
+                type: 'confirm',
+                name: 'firstQuestion',
+                default: false,
+            },
+            {
+                message: randomQuestion(answers.avmType, 'bloque2'),
+                type: 'confirm',
+                name: 'secondQuestion',
+                default: false,
+            }];
+
+            prompt(typeQuestions)
+                .then(answers => {
+                    waitaMoment();
+            }).catch(error => {
+                throw error;
+            });
+
+
+        }).catch(error => {
+            throw error;
+        });
+
+
+
     })
+    .catch( error => {
+        throw error;
+    });
     
 }
