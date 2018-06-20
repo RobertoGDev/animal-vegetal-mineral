0803c8336ae4f4885e362c0499624173cdb3da41
Roberto <rob.vicalvaro@gmail.com>
2018-06-21 00:47:17 +0200

    Cambios en la mecánica de juego, resuelve tras la primera respuesta.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const prompt = inquirer.createPromptModule();
 const json = fs.readFileSync("data_base.json");
 const database = JSON.parse(json);
 
+
+
+
+
 function randomQuestion(type, block) {
     
     let info = database[type].preguntas[block];
@@ -12,36 +16,59 @@ function randomQuestion(type, block) {
 
     let questionNum = Math.floor(Math.random() * size.length);
     
-    let index = [];
-    
-    index.sort(function (a, b) {
-        return a == b ? 0 : (a > b ? 1 : -1);
-    });
+    let question = {};
+    question.numQuestion = questionNum;
+    question.msgQuestion = info[questionNum];
 
-    for (var x in info) {
-        index.push(x);
-    }
-    return info[index[questionNum]];
+	return question;
 }
+
 
 function waitaMoment() {
 
         var P = ["\\", "|", "/", "-"];
         var x = 0;
-        console.log('Dejame pensar!');
+        console.log('Ok dejame pensar!');
         return setInterval(function () {
             process.stdout.write("\r" + P[x++]);
-            x &= 3;
-        }, 250);
+            x = x % P.length;
+        }, 100);
 
 }
+
+function resolveFormula(generatedQuestion, type, database) {
+	
+	let Qvalue = generatedQuestion.numQuestion;
+	let obj = database[type].respuestas;
+	
+	let posibleAnswers = [];
+
+	//recogemos en un array todas las respuestas que contengan al valor
+	Object.keys(obj).forEach(function (key) {
+		if (obj[key].indexOf(Qvalue) ) {
+			posibleAnswers.push(key);
+		}
+	});
+
+	var size = Object.getOwnPropertyNames(obj);
+	let questionNum = Math.floor(Math.random() * size.length);
+
+	return posibleAnswers[questionNum];
+
+}
+
+
+function surrender() {
+	console.log('No doy más de mí');
+}
+
 
 // Creamos el inicio del juego
 // Por cada elemento, hay dos bloques, preguntas y respuestas,
 // Las preguntas tienen dos bloques primero lanza una pregunta del primer bloque que filtra un gran número de respuestas y despues lanza una pregunta aleatoria del segundo bloque que define mejor la respuesta
 
 
-// Bienvenida
+// Bienvenida y continuar si acepta el jugador
 
 let wellcomeQuestion = [{
     message: 'Bienvenido al juego Animal Vegetal Mineral, ¿Quiere jugar?',
@@ -50,31 +77,26 @@ let wellcomeQuestion = [{
     name: 'letsPlay'
 }];
 
+prompt(wellcomeQuestion).then(answers => {
+    if (!answers.letsPlay) {
+        console.log(`Vuelve cuando quieras! :)`);
+        return;
+    }
+    // Obtenemos el jugador
+
+    let continuePlay = [{
+        message: 'Por favor, dígame su nombre',
+        type: 'input',
+        name: 'namePlayer'
+    }];
+
+    continueExecution(continuePlay);
+}).catch( error => {
+    throw error;
+});
 
 
-
-prompt(wellcomeQuestion)
-    .then(answers => {
-        if (!answers.letsPlay) {
-            console.log(`Vuelve cuando quieras! :)`);
-            return;
-        }
-        // Obtenemos el jugador
-
-        let continuePlay = [{
-            message: 'Por favor, dígame su nombre',
-            type: 'input',
-            name: 'namePlayer'
-        }];
-
-        continueExecution(continuePlay);
-    })
-    .catch( error => {
-        throw error;
-    });
-
-
-// Seguimos con el juego
+// Si el jugador acepta seguimos con el juego
 
 function continueExecution(continuePlay) {
 
@@ -87,10 +109,12 @@ function continueExecution(continuePlay) {
         }
 
         // Bienvenido!
-        console.log(`Bienvenido al juego ${answers.namePlayer}! 
-Con solo dos preguntas voy a adivinar lo que estás pensando. 
-Comencemos con la primera pregunta...`);
+        console.log(`Bienvenid@ al juego ${answers.namePlayer}! \r\nEl juego trata de averiguar lo que estas pensando con algunas preguntas sencillas. \r\nPero primero:`);
         
+
+
+
+
 
         // Preguntamos por animal vegetal mineral
 
@@ -103,29 +127,74 @@ Comencemos con la primera pregunta...`);
         
 
         prompt(avmQuestion)
-            .then(answers => {
+            .then(answersType => {
+                let generatedFirst = randomQuestion(answersType.avmType, 'bloque1');
 
             // Generamos y lanzamos la pregunta aleatoria del bloque 1
-            let typeQuestions = [{
-                message: randomQuestion(answers.avmType, 'bloque1'),
+            let firstQuestion = [{
+                message: generatedFirst.msgQuestion,
                 type: 'confirm',
-                name: 'firstQuestion',
-                default: false,
-            },
-            {
-                message: randomQuestion(answers.avmType, 'bloque2'),
-                type: 'confirm',
-                name: 'secondQuestion',
+                name: 'firstq',
                 default: false,
             }];
 
-            prompt(typeQuestions)
-                .then(answers => {
-                    waitaMoment();
+            prompt(firstQuestion)
+                .then((answer) => {
+
+                    //Analiza la primera pregunta y prueba suerte
+
+                    if (answer.firstq) {
+                        
+                        let loading = waitaMoment();
+                        setTimeout(() => {
+							clearInterval(loading);
+							
+							let resolveOne = [{
+								message: 'Me la juego, ¿Es un '+resolveFormula(generatedFirst, answersType.avmType, database),
+								type: 'confirm',
+								name: 'resolveOne',
+								default: false,
+							}];
+							
+							prompt(resolveOne)
+								.then((answer) => {
+									if (!answer.resolveOne) {
+										surrender();
+									} else {
+										console.log('Estupendo!! Ya conozco otra respuesta más, siento no poder registrarte como autor, pero mi programador se va a volver loco como le pida que registre autores :___( ')
+									}
+								}).catch(error => {
+									throw error;
+								});
+							
+                        }, 2500);
+                    } else {
+                        let generatedSecond = randomQuestion(answersType.avmType, 'bloque2');
+                        // En caso de se no, gGeneramos y lanzamos la pregunta aleatoria del bloque 2 
+                        let secondQuestion = [{
+                            message: generatedSecond.msgQuestion,
+                            type: 'confirm',
+                            name: 'secondQ',
+                            default: false,
+                        }];
+
+                        prompt(secondQuestion)
+                            .then((secondAnswer) => {
+                                let loading = waitaMoment();
+                                setTimeout(() => {
+                                    clearInterval(loading);
+                                    resolveFormula(secondAnswer, answersType.avmType, database);
+                                }, 2500);
+                            }).catch(error => {
+                                throw error;
+                            });
+
+                    }
+
+                   
             }).catch(error => {
                 throw error;
             });
-
 
         }).catch(error => {
             throw error;
